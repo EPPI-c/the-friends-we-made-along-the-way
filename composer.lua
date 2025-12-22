c26463a6cda5515457c390fa0beae93274a81a72
EPPI <109323250+EPPI-c@users.noreply.github.com>
2025-12-22 00:07:34 -0300

    add composer

---
+++ composer.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+function M.init(level)
+    self.lines = {}
+    for _, measure in pairs(self.level.measures) do
+	for _, line in pairs(measure.notes) do
+            table.insert(self.lines, line)
+        end
+    end
+end
+
+function M:updateMeasures(songPosition)
+    local oneth = self.lines[1]
+    local second = self.lines[2]
+    if not oneth or not second then return end
+    if math.abs(songPosition - oneth.lastbeat)
+	    >
+	math.abs(songPosition - second.lastbeat) then
+	table.remove(self.lines, 1)
+    end
+end
+
+return M
